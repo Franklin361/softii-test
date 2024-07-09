73b7b5c51525c66192813a31a3198d24b84a674a
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2024-07-09 17:13:50 -0600

    style(frontend): :lipstick: update styles

---
+++ frontend/src/components/TipPaymentOptions.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import TotalTipAmount from './TotalTipAmount.vue'
+import TipPerPerson from './TipPerPerson.vue'
+import PaymentMethod from './PaymentMethod.vue'
+</script>
+
+<template>
+  <section class=" max-w-sm mx-auto">
+    <!--  -->
+    <TotalTipAmount />
+    <!--  -->
+    <TipPerPerson />
+    <!--  -->
+    <PaymentMethod />
+    <!--  -->
+  </section>
+</template>
